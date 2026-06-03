98617f07120562df695fe71a5d634f88c4e899a5
jbrenesp <josembrenes@gmail.com>
2026-06-03 11:40:11 +0300

    Added rspec tests to Illness Model

--- spec/models/illness_spec.rb
+++ spec/models/illness_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Illness, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:family_member) }
+    it { should have_many(:doctor_visits).dependent(:destroy) }
+    it { should have_many(:medications).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it 'is invalid without a name' do
+      illness = Illness.new(name: nil)
+      expect(illness).not_to be_valid
+    end
+
+    it 'is invalid without a start_date' do
+      illness = Illness.new(start_date: nil)
+      expect(illness).not_to be_valid
+    end
+
+    it 'is valid with a name and start_date' do
+      family = Family.create(name: Faker::Name.last_name)
+      family_member = FamilyMember.create(name: Faker::Name.first_name, member_type: 'adult', is_private: false, family: family)
+      illness = Illness.new(name: Faker::Lorem.word, start_date: Date.today, family_member: family_member)
+      expect(illness).to be_valid
+    end
+  end
 end
