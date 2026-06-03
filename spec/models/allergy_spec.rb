852ae7238174d5bc6821140f5a665bf6979f1b98
jbrenesp <josembrenes@gmail.com>
2026-06-03 12:32:23 +0300

    Add Allergy RSpec tests

--- spec/models/allergy_spec.rb
+++ spec/models/allergy_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Allergy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:family_member) }
+  end
+
+  describe 'validations' do
+    it 'is invalid without a name' do
+      allergy = Allergy.new(name: nil)
+      expect(allergy).not_to be_valid
+    end
+
+    it 'is invalid without a severity' do
+      allergy = Allergy.new(severity: nil)
+      expect(allergy).not_to be_valid
+    end
+
+    it 'is invalid without an allergy_type' do
+      allergy = Allergy.new(allergy_type: nil)
+      expect(allergy).not_to be_valid
+    end
+
+    it 'is invalid with incorrect severity' do
+      allergy = Allergy.new(severity: 'extreme')
+      expect(allergy).not_to be_valid
+    end
+
+    it 'is valid with required fields' do
+      family = Family.create(name: Faker::Name.last_name)
+      family_member = FamilyMember.create(name: Faker::Name.first_name, member_type: 'adult', is_private: false, family: family)
+      allergy = Allergy.new(name: Faker::Lorem.word, allergy_type: 'food', severity: 'moderate', family_member: family_member)
+      expect(allergy).to be_valid
+    end
+  end
 end
