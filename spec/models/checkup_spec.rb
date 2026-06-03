407c95cf608fb0e76b9c776071a80f9106b7688d
jbrenesp <josembrenes@gmail.com>
2026-06-03 17:39:44 +0300

    Added Checkup rspec tests

--- spec/models/checkup_spec.rb
+++ spec/models/checkup_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Checkup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:family_member) }
+  end
+
+  describe 'validations' do
+    it 'is invalid without a date' do
+      checkup = Checkup.new(date: nil)
+      expect(checkup).not_to be_valid
+    end
+
+    it 'is invalid without a checkup_type' do
+      checkup = Checkup.new(checkup_type: nil)
+      expect(checkup).not_to be_valid
+    end
+
+    it 'is invalid with incorrect checkup_type' do
+      checkup = Checkup.new(checkup_type: 'blood_general')
+      expect(checkup).not_to be_valid
+    end
+
+    it 'is valid with required fields' do
+      family = Family.create(name: Faker::Name.last_name)
+      family_member = FamilyMember.create(name: Faker::Name.first_name, member_type: 'adult', is_private: false, family: family)
+      checkup = Checkup.new(checkup_type: 'general', date: Date.today, family_member: family_member)
+      expect(checkup).to be_valid
+    end
+  end
 end
