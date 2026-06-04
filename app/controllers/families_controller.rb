31d89ccb6d3d8bc12b1dbcc7a62deb9ad673abf2
jbrenesp <josembrenes@gmail.com>
2026-06-04 16:00:40 +0300

    Implement FamiliesController CRUD actions

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,19 +1,51 @@
 class FamiliesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_family, only: [ :show, :edit, :update, :destroy ]
+
+
+
   def show
+    @family_members = @family.family_members
+    @users = @family.users
   end
 
   def new
+    @family = Family.new
   end
 
   def create
+    @family = Family.new(family_params)
+    if @family.save
+      current_user.update(family: @family)
+      redirect_to @family, notice: "Family was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @family.update(family_params)
+      redirect_to @family, notice: "Family was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @family.destroy
+    redirect_to root_path, notice: "Family was successfully deleted."
+  end
+
+  private
+
+  def set_family
+    @family = Family.find(params[:id])
+  end
+
+  def family_params
+    params.require(:family).permit(:name)
   end
 end
