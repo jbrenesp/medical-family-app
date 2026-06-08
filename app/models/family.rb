60d8c0a6cb6377775b2de54904e4f359ae274445
jbrenesp <josembrenes@gmail.com>
2026-06-08 15:13:53 +0300

    Fix Family dependent nullify to prevent deletion blocking

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,6 @@
 class Family < ApplicationRecord
   has_many :family_members, dependent: :destroy
-  has_many :users
+  has_many :users, dependent: :nullify
 
   validates :name, presence: true
 end
