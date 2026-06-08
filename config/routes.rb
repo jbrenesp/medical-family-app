6045b72da60fac523902e7ccfc82ebf24d0986e3
jbrenesp <josembrenes@gmail.com>
2026-06-08 15:15:18 +0300

    Add FamilyMembers generated files and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :families, only: [ :show, :new, :create, :edit, :update, :destroy ]
+  resources :families, only: [ :show, :new, :create, :edit, :update, :destroy ] do
+    resources :family_members, only: [ :show, :new, :create, :edit, :update, :destroy ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
