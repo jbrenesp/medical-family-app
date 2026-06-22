a68e5575748fb508f35e1f2fc989ea49a42c2f8c
jbrenesp <josembrenes@gmail.com>
2026-06-22 14:02:24 +0300

    Add family members request specs and update factory

--- spec/factories/family_members.rb
+++ spec/factories/family_members.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :family_member do
-    family { nil }
+    association :family
     user { nil }
-    name { "MyString" }
+    name { Faker::Name.first_name }
+    member_type { "adult" }
+    date_of_birth { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    blood_type { "A+" }
+    is_private { false }
   end
 end
--- spec/requests/family_members_spec.rb
+++ spec/requests/family_members_spec.rb
@@ -1,7 +1,82 @@
 require 'rails_helper'
 
-RSpec.describe "FamilyMembers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe FamilyMembersController, type: :request do
+  let(:user) { create(:user) }
+  let(:family) { create(:family) }
+  let(:family_member) { create(:family_member, family: family) }
+
+  before do
+    user.update(family: family)
+    sign_in user
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get family_family_member_path(family, family_member)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get new_family_family_member_path(family)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a family member and redirects" do
+        post family_family_members_path(family), params: { family_member: { name: "John", member_type: "adult", date_of_birth: "1990-01-01", blood_type: "A+", is_private: false } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid params" do
+      it "re-renders new" do
+        post family_family_members_path(family), params: { family_member: { name: "" } }
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get edit_family_family_member_path(family, family_member)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid params" do
+      it "updates and redirects" do
+        patch family_family_member_path(family, family_member), params: { family_member: { name: "Updated Name" } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid params" do
+      it "re-renders edit" do
+        patch family_family_member_path(family, family_member), params: { family_member: { name: "" } }
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys and redirects" do
+      delete family_family_member_path(family, family_member)
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "IDOR protection" do
+    let(:other_family) { create(:family) }
+    let(:other_family_member) { create(:family_member, family: other_family) }
+
+    it "cannot access another family's family member" do
+      get family_family_member_path(other_family, other_family_member)
+      expect(response).not_to have_http_status(:success)
+    end
   end
 end
