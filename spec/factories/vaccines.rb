faca627a76635741f96ebb6263e4ca5f7512ed6c
jbrenesp <josembrenes@gmail.com>
2026-06-24 14:58:53 +0300

    Add rspec test for vaccine and update factories

--- spec/factories/vaccines.rb
+++ spec/factories/vaccines.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do
   factory :vaccine do
-    family_member { nil }
-    name { "MyString" }
+    association :family_member
+    name { Faker::Name.first_name }
     vaccine_date { "2026-06-01" }
-    next_due_date { "2026-06-01" }
-    dose_number { 1 }
-    notes { "MyText" }
   end
 end
--- spec/requests/vaccines_spec.rb
+++ spec/requests/vaccines_spec.rb
@@ -1,46 +1,84 @@
 require 'rails_helper'
 
-RSpec.describe "Vaccines", type: :request do
-  describe "GET /show" do
+RSpec.describe MedicationsController, type: :request do
+  let(:user) { create(:user) }
+  let(:family) { create(:family) }
+  let(:family_member) { create(:family_member, family: family) }
+  let(:vaccine) { create(:vaccine, family_member: family_member) }
+
+  before do
+    user.update(family: family)
+    sign_in user
+  end
+
+  describe "GET #show" do
     it "returns http success" do
-      get "/vaccines/show"
+      get family_family_member_vaccine_path(family, family_member, vaccine)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
+  describe "GET #new" do
     it "returns http success" do
-      get "/vaccines/new"
+      get new_family_family_member_vaccine_path(family, family_member)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a vaccine and redirects" do
+        post family_family_member_vaccines_path(family, family_member), params: { vaccine: { name: "Influenza", vaccine_date: "2026-01-01" } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid params" do
+      it "re-renders new" do
+        post family_family_member_vaccines_path(family, family_member), params: { vaccine: { name: "" } }
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+    end
+  end
+
+  describe "GET #edit" do
     it "returns http success" do
-      get "/vaccines/edit"
+      get edit_family_family_member_vaccine_path(family, family_member, vaccine)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/vaccines/create"
-      expect(response).to have_http_status(:success)
+  describe "PATCH #update" do
+    context "with valid params" do
+      it "updates and redirects" do
+        patch family_family_member_vaccine_path(family, family_member, vaccine), params: { vaccine: { name: "Updated Name" } }
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid params" do
+      it "re-renders edit" do
+        patch family_family_member_vaccine_path(family, family_member, vaccine), params: { vaccine: { name: "" } }
+        expect(response).to have_http_status(:unprocessable_content)
+      end
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/vaccines/update"
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    it "destroys and redirects" do
+      delete family_family_member_vaccine_path(family, family_member, vaccine)
+      expect(response).to redirect_to(family_family_member_path(family, family_member))
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/vaccines/destroy"
-      expect(response).to have_http_status(:success)
+  describe "IDOR protection" do
+    let(:other_family) { create(:family) }
+    let(:other_family_member) { create(:family_member, family: other_family) }
+    let(:other_vaccine) { create(:vaccine, family_member: other_family_member) }
+
+    it "cannot access another family's vaccine" do
+      get family_family_member_vaccine_path(other_family, other_family_member, other_vaccine)
+      expect(response).not_to have_http_status(:success)
     end
   end
-
 end
